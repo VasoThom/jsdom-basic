eebf6ca9fc1f3fc17abca5a4ca10d100dfe4cfe3
Vaso <vasothom@hotmail.com>
2022-09-25 14:53:20 +0200

    buttons

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-//console.log("Starting Javascript");
+console.log("Starting Javascript");
 const messageEl = document.querySelector(".message");
 const clickButtonEL = document.querySelector(".clickButton");
+clickButtonEL.style.backgroundColor = "red";
 
 messageEl.innerHTML = "This is <b>Javascript</b>";
 clickButtonEL.addEventListener("click", () => {
-  //console.log("butoon we clicked");
+  clickButtonEL.style.backgroundColor = "blue";
+  console.log("butoon we clicked");
   messageEl.innerHTML =
-    "This is <b>Javascript</b> and i changed it with one click";
+    "This is <b>JAVASCRIPT</b> and i changed it with ONE click";
+  messageEl.style.color = "yellow";
+});
+console.log("-------------2------------");
+const questionEl = document.querySelector(".frage");
+const answerEl = document.querySelector(".antwort");
+const buttonanswerEl = document.querySelector(".show-answer");
+buttonanswerEl.style.backgroundColor = "blue";
+buttonanswerEl.style.color = "white";
+
+buttonanswerEl.addEventListener("click", () => {
+  //answerEl.style.display = "block";
+  answerEl.classList.toggle("hidden");
+  answerEl.style.color = "green";
 });
